2c8bba03696eb90e44fa8d86a838e0a5018872c4
anilcyberlink <anilcyberlink@gmail.com>
2026-06-17 17:04:42 +0545

    mail correct

--- app/Mail/SendNewsletter.php
+++ app/Mail/SendNewsletter.php
@@ -3,39 +3,32 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Http\Request;
 use App\Model\Newsletter;
 
 class SendNewsletter extends Mailable
 {
     use Queueable, SerializesModels;
 
-    public $user;
+    public $newsletter;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct($user)
+    public function __construct(Newsletter $newsletter)
     {
-      $this->user = $user;
-
+        $this->newsletter = $newsletter;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build(Request $request)
+    public function build()
     {
-         $news=Newsletter::where('id',$request->news_id)->first();
-        $title=$news->title;
-        $content=$news->content;
-        return $this->view('emails.Test_mail',['title'=>$title,'content' => $content])->subject($title);;
+        return $this->replyTo(
+                'lhakpatrekking@gmail.com',
+                'Lhakpa Trekking'
+            )
+            ->subject($this->newsletter->title)
+            ->view('emails.Test_mail')
+            ->with([
+                'title'   => $this->newsletter->title,
+                'content' => $this->newsletter->content,
+            ]);
     }
 }
